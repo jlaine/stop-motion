8523d7b201e108d25f26ce581c214d8c2d5cf60c
Jeremy Lainé <jeremy.laine@m4x.org>
2020-04-20 08:16:43 +0200

    add a Camera class

--- capture.py
+++ capture.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
 import logging
 import os
 import sys
 
-import gphoto2 as gp
+from src.camera import FfmpegCamera, Gphoto2Camera
 
-output_dir = 'images'
-output_tpl = 'capt%.4d.jpg'
+output_dir = "images"
+output_tpl = "capt%.4d.jpg"
 
 
 def next_filename():
@@ -25,36 +23,26 @@ def next_filename():
 
 def main():
     logging.basicConfig(
-        format='%(levelname)s: %(name)s: %(message)s', level=logging.WARNING)
+        format="%(levelname)s: %(name)s: %(message)s", level=logging.INFO
+    )
 
     # create directory
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # open camera
-    gp.check_result(gp.use_python_logging())
-    camera = gp.check_result(gp.gp_camera_new())
-    try:
-        gp.check_result(gp.gp_camera_init(camera))
-    except gp.GPhoto2Error as exc:
-        logging.warning("Could not open camera: %s", exc)
-        sys.exit(1)
+    camera = Gphoto2Camera()
 
     while True:
         try:
-            input('Press a key to capture image')
+            input("Press a key to capture image")
         except KeyboardInterrupt:
             return
 
-        print('Capturing image')
-        file_path = gp.check_result(gp.gp_camera_capture(
-            camera, gp.GP_CAPTURE_IMAGE))
         target = next_filename()
-        print('Copying image to', target)
-        camera_file = gp.check_result(gp.gp_camera_file_get(
-            camera, file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL))
-        gp.check_result(gp.gp_file_save(camera_file, target))
-    gp.check_result(gp.gp_camera_exit(camera))
+        logging.info("Capturing image to %s", target)
+        camera.capture(target)
+
+    camera.close()
 
 
 if __name__ == "__main__":
---
+++ src/camera.py
@@ -0,0 +1,35 @@
+import subprocess
+
+import gphoto2 as gp
+
+
+class FfmpegCamera:
+    def capture(self, path):
+        subprocess.check_call(
+            ["ffmpeg", "-f", "v4l2", "-i", "/dev/video0", "-frames:v", "1", path]
+        )
+
+    def close(self):
+        pass
+
+
+class Gphoto2Camera:
+    def __init__(self):
+        gp.check_result(gp.use_python_logging())
+
+        self._camera = gp.check_result(gp.gp_camera_new())
+        gp.check_result(gp.gp_camera_init(self._camera))
+
+    def capture(self, path):
+        file_path = gp.check_result(
+            gp.gp_camera_capture(self._camera, gp.GP_CAPTURE_IMAGE)
+        )
+        camera_file = gp.check_result(
+            gp.gp_camera_file_get(
+                self._camera, file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL
+            )
+        )
+        gp.check_result(gp.gp_file_save(camera_file, path))
+
+    def close(self):
+        gp.check_result(gp.gp_camera_exit(self._camera))
